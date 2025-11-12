68a73380820d748971693b52c5b65579007afe2b
EmilioCueULL <alu0101117904@ull.edu.es>
2025-11-12 05:31:31 +0000

    feat(mountain): productos concretos y MountainExpeditionFactory completa

---
+++ src/core/products/impl/mountain/MountainSuspension.java
@@ -0,0 +1,10 @@
+package core.products.impl.mountain;
+
+import core.products.Suspension;
+
+public class MountainSuspension implements Suspension {
+    @Override
+    public String getSuspensionType() {
+        return "Suspensión elevada con amortiguadores reforzados";
+    }
+}
---
+++ src/core/products/impl/mountain/MountainTire.java
@@ -0,0 +1,10 @@
+package core.products.impl.mountain;
+
+import core.products.Tire;
+
+public class MountainTire implements Tire {
+    @Override
+    public String getTreadType() {
+        return "Neumáticos All-Terrain (agarre en roca, barro y asfalto)";
+    }
+}
